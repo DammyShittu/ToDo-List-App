35dfa1416dd38a366cbb9e62c90125438d078f06
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-08-21 03:13:00 +0100

    Add Code To Edit Tasks On Double Click

--- src/showTasks.js
+++ src/showTasks.js
@@ -7,45 +7,45 @@ const showTasks = () => {
   listContainer.innerHTML = '';
   const tasks = getTasksFromLocalStorage();
   for (let i = 0; i < tasks.length; i += 1) {
-    const list = ` <li class="task" id="${tasks[i].index}" data-key="${tasks[i].index}" draggable="true">
+    const holder = document.createElement('div');
+    holder.innerHTML += ` <li class="task" id="${tasks[i].index}" data-key="${tasks[i].index}" draggable="true">
     <div>
-      <input type="checkbox" class="box" id="list-box" name="list-box">
-      <label class="form-label">${tasks[i].description}</label>
+      <input id="box-${i}" type="checkbox" class="box" id="list-box" name="list-box">
+      <label id="label-${i}" class="form-label">${tasks[i].description}</label>
     </div>
   
     <button id="${tasks[i].index}" class="delete"><i class='fas fa-trash-alt delete-task'></i></button>
   </li>`;
 
-    listContainer.innerHTML += list;
-  }
+    while (holder.firstChild) {
+      listContainer.appendChild(holder.firstChild);
+    }
 
-  // Add Line Through When Checkbox Is Checked
-  const box = document.querySelectorAll('.box');
-  for (let j = 0; j < box.length; j += 1) {
-    box[j].checked = tasks[j].completed;
-    box[j].addEventListener('change', (event) => {
-      completeTask(event.target, tasks[j]);
+    const label = document.getElementById(`label-${i}`);
+    const box = document.getElementById(`box-${i}`);
+
+    box.checked = tasks[i].completed;
+    box.addEventListener('change', (event) => {
+      completeTask(event.target, tasks[i]);
       setLocalStorage(tasks);
     });
-  }
 
-  const label = document.querySelectorAll('.form-label');
-  label.forEach((item) => {
-    item.addEventListener('dblclick', () => {
-      item.setAttribute('contenteditable', 'true');
+    label.addEventListener('dblclick', () => {
+      label.setAttribute('contenteditable', 'true');
     });
 
     // Show Delete Icon On Focus
-    item.addEventListener('focus', () => {
-      item.parentElement.parentElement.style.backgroundColor = '#dadadc';
+    label.addEventListener('focus', () => {
+      label.parentElement.parentElement.style.backgroundColor = '#dadadc';
     });
 
     // Hide Delete Icon On Blur
-    item.addEventListener('blur', () => {
-      item.parentElement.parentElement.style.backgroundColor = '#fff';
+    label.addEventListener('blur', () => {
+      label.parentElement.parentElement.style.backgroundColor = '#fff';
+      tasks[i].description = label.innerHTML;
       setLocalStorage(tasks);
     });
-  });
+  }
 };
 
 export default showTasks;
